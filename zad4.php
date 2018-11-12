cdd900846e9b5a37203a3bff9bdb2f2c07a022b6
wojtekzurek <306990@uwr.edu.pl>
2018-11-12 14:37:22 +0100

    Update zad4.php

---
+++ zad4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lnag="pl">
+<head>
+	<meta charset="UTF-8">
+	<title>zad4</title>
+</head>
+<body>
+
+	<?php
+		for($x=1; $x<=1000; $x++){
+			echo "$x "
+		}
+	?>
+	
+</body>
+</html>
